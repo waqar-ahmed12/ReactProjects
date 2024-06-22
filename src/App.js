4312c9611a065576606a2eece1054c7a23249325
waqar-ahmed12 <118617833+waqar-ahmed12@users.noreply.github.com>
2024-06-22 17:54:28 +0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import Accordion from './accordion.js';
+import Mode from "./Mode.js";
+import data from "./data.js";
 
-function App() {
+function App() 
+{
+  const [background, setBackground] = useState("#808080");
+  const [foreground, setForeground] = useState("white");
+
+  React.useEffect(() => 
+  {
+    document.body.style.backgroundColor = background;
+    document.body.style.color = foreground;
+  }, [background]);
+
+
+  function changeBackground() 
+  {
+    let back = background == "white" ? "#808080" : "white";
+    let fore = foreground == "#808080" ? "white" : "#808080";
+    setBackground(back);
+    setForeground(fore);
+  }
+
+  const [status, setStatus] = useState([]);
+  
+
+  //For the first time, we add expanded property to the data
+  //and set data to useEffect
+  useEffect(() =>{
+    data.forEach((items) => items.expanded = false);
+    
+    setStatus(data)
+  }, [])
+
+  const expand = (id) => 
+  {
+    //Methods to expand or collapse
+
+    // let obj = {...status}
+    // if(data[id].id == id + 1)
+    // {     
+    //   obj[id].expanded = !obj[id].expanded ? true : false
+    //   setStatus(obj)
+    // }
+
+    
+    setStatus(oldData => oldData.map(
+      item => 
+        {
+          return item.id == (id) ? 
+          {...item, expanded: item.expanded ? false : true} : 
+          item
+        }))
+  }
+
+
+  const AccordionArray = status.map((item) => 
+     <Accordion key={item.id} question={item.question} answer={item.answer} expand={expand} id={item.id} expanded={item.expanded}/>
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Change Background Color</h1>
+
+      <Mode back={background} change={changeBackground}/>
+
+      <hr />
+
+      {AccordionArray}
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,19 @@
+const data = 
+[
+    {
+        id: 1,
+        question: 'what is the name',
+        answer: 'this is the name'
+    },
+    {
+        id: 2,
+        question: 'where is the name',
+        answer: 'here is the name'
+    },
+    {
+        id: 3,
+        question: 'how is the name',
+        answer: 'like this is the name'
+    },
+]
+export default data;
